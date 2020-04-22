39b5325cbc432403c3565727dae2643281c78f75
abdul mudhir <abdulmudhirss@gmail.com>
2020-04-22 14:56:10 +0100

    added 3 pages to scrape and loop to check every hour for any new games

--- ggdeals.py
+++ ggdeals.py
@@ -23,64 +23,60 @@ class GGDeals(commands.Cog):
     async def on_ready(self):
         print(f'We have logged in as {self.bot.user}\n')
         self.channel = self.bot.get_channel(702208238149435533)
-        await self.send_deals()
-        await self.remove_outdated_deals()
+        self.start_sending.start()
 
     def deals(self):
         deals_found = {}
 
-        ggdeals_best_deal = requests.get(self.link, headers=header).content
+        for page_number in range(1, 3):
 
-        soup = bs4(ggdeals_best_deal, 'html.parser')
+            ggdeals_best_deal = requests.get(f"https://gg.deals/deals/best-deals/?page={page_number}",
+                                             headers=header).content
 
-        find_deal = soup.find_all('div', class_='deal-list-item')
+            soup = bs4(ggdeals_best_deal, 'html.parser')
 
-        for deal in find_deal:
-            title = deal.find_next('a', class_="ellipsis title").text
-            current_price = deal.find_next('span', class_="numeric").text
-            previous_price = deal.find_next('span', class_="bottom").text
-            direct_link = f"https://gg.deals{deal.find_next('a', class_='shop-link').get('href')}"
-            platform = deal.find_next('a', class_="shop-link").img.get('alt')
-            deal_posted_date = deal.find_next('div', class_='time-tag').span.text
-            genre = deal.find_next('div', class_='tag- ellipsis tag with-bull').span.text
+            find_deal = soup.find_all('div', class_='deal-list-item')
 
-            # get img tag from the html to check if  links are in the right attributes
-            image_class = deal.find_next('a', class_="main-image").img.get('class')
+            for deal in find_deal:
 
-            if not self.check_lazy_load(image_class):
-                # check if the image link are been hidden
-                game_image = deal.find_next('a', class_="main-image").img.get('src')
-                platform_image = deal.find_next('a', class_="shop-link").img.get('src')
+                title = deal.find_next('a', class_="ellipsis title").text
+                current_price = deal.find_next('span', class_="numeric").text
+                previous_price = deal.find_next('span', class_="bottom").text
+                direct_link = f"https://gg.deals{deal.find_next('a', class_='shop-link').get('href')}"
+                platform = deal.find_next('a', class_="shop-link").img.get('alt')
+                deal_posted_date = deal.find_next('div', class_='time-tag').span.text
+                genre = deal.find_next('div', class_='tag- ellipsis tag with-bull').span.text
 
-            else:
-                game_image = deal.find_next('a', class_="main-image").img.get('data-src')
-                platform_image = deal.find_next('a', class_="shop-link").img.get('data-src')
+                # get img tag from the html to check if  links are in the right attributes
+                image_class = deal.find_next('a', class_="main-image").img.get('class')
 
-            deals_found[title] = {'price': current_price,
-                                  'p_price': previous_price,
-                                  'direct_link': direct_link,
-                                  'platform': platform,
-                                  'game_image': game_image,
-                                  'platform_image': platform_image,
-                                  'date_posted': deal_posted_date,
-                                  'genre': genre}
+                if not self.check_lazy_load(image_class):
+                    # check if the image link are been hidden
+                    game_image = deal.find_next('a', class_="main-image").img.get('src')
+                    platform_image = deal.find_next('a', class_="shop-link").img.get('src')
+
+                else:
+                    game_image = deal.find_next('a', class_="main-image").img.get('data-src')
+                    platform_image = deal.find_next('a', class_="shop-link").img.get('data-src')
+
+                deals_found[title] = {'price': current_price,
+                                      'p_price': previous_price,
+                                      'direct_link': direct_link,
+                                      'platform': platform,
+                                      'game_image': game_image,
+                                      'platform_image': platform_image,
+                                      'date_posted': deal_posted_date,
+                                      'genre': genre}
 
         return deals_found
 
     async def get_posted_deals(self):
 
-        chat_logs = {}
+        messages = await self.channel.history(limit=50).flatten()
+        # get the title of each embeds and the message id
+        posted_deals = {message.embeds.pop().title: message.id for message in messages}
 
-        msg = await self.channel.history(limit=1000).flatten()
-
-        for messages in msg:
-
-            embed_content = messages.embeds
-            for content in embed_content:
-                title = content.title  # title of the embed
-                chat_logs[title] = messages.id
-
-        return chat_logs
+        return posted_deals
 
     async def send_deals(self):
         # get the posted deals
@@ -91,13 +87,13 @@ class GGDeals(commands.Cog):
         for game_title, game_info in self.current_deals.items():
 
             if game_title not in self.posted_deals:
-
                 price = game_info.get('price')
 
+                price_formatted = f'```yaml\n{price}```' if price == "Free" else price
 
                 embed = discord.Embed(
                     title=game_title,
-                    description=f" **Price:** {game_info.get('price')}\n"
+                    description=f" **Price:** {price_formatted}\n"
                                 f"**Platform:** {game_info.get('platform')}\n"
                                 f" **Genre:** {game_info.get('genre')}",
                     colour=2470660,
@@ -108,16 +104,23 @@ class GGDeals(commands.Cog):
         # update the posted deals that have been sent across
         self.posted_deals = await self.get_posted_deals()
 
+    @tasks.loop(hours=1)
+    async def start_sending(self):
+        await self.send_deals()
+        await self.remove_outdated_deals()
+
     async def remove_outdated_deals(self):
 
         for post, post_id in self.posted_deals.items():
+
             if post not in self.current_deals:
-                # get the mesage
-                get_message = self.channel.fetch_message(post_id)
+                # get the message that was sent
+                get_message = await self.channel.fetch_message(post_id)
                 # delete the message as the deal most likely expired
                 await get_message.delete()
 
-
+        # reset the deals found on gg.deals
+        self.current_deals = ''
 
     @staticmethod
     def check_lazy_load(class_):
